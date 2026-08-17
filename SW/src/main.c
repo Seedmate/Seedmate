bbd1cc817e151d923c3b2d05825b86f6e3c0218d
Seedmate <github@seedmate.net>
2026-08-17 17:18:34 +0200

    Update copyright holder in main.c

--- SW/src/main.c
+++ SW/src/main.c
@@ -32,7 +32,7 @@
  * 
  * MIT License
  * 
- * Copyright (c) 2026 [Tu Nombre o Nombre de la Empresa]
+ * Copyright (c) 2026 Seedmate
  * 
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
